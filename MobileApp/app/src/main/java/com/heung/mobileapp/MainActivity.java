f4e1e9da8886c287d15bd598ac1bac1bbc846f22
jhwan7 <jhwan7@uwo.ca>
2019-11-23 04:03:18 -0500

    Title UI Screen Complete

--- MobileApp/app/src/main/java/com/heung/mobileapp/MainActivity.java
+++ MobileApp/app/src/main/java/com/heung/mobileapp/MainActivity.java
@@ -1,14 +1,29 @@
 package com.heung.mobileapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try{
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){
+            setContentView(R.layout.activity_main);
+        }
+        setStatusBarColor(Color.BLACK);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void setStatusBarColor (int color){
+        getWindow().setStatusBarColor(color);
     }
 }
